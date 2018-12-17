61b8e1b3dd89a7eda9c10f1de9fd8556d9fcfef3
Sara Walker <sarawall1187@gmail.com>
2018-12-17 17:16:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
-def prime?(max)
-  i = 2
-  while i < max
-    is_divisible = ((max % i) == 0)
-    if is_divisible
-      return false
-    end
-    i += 1
-  end
- true 
-end
+# def prime?(max)
+#   i = 2
+#   while i < max
+#     is_divisible = ((max % i) == 0)
+#     if is_divisible
+#       return false
+#     end
+#     i += 1
+#   end
+# true 
+# end
