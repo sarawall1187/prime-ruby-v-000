3c2ff601d6f786ca7fc141fade9ddf23ae17ab85
Sara Walker <sarawall1187@gmail.com>
2018-12-17 17:18:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,19 +1,10 @@
-def prime?(int)
-  if int < 2 
-    (2..int - 1).each do |x|
-      if (int % x) == 0 
-        return false 
-      end
-    end
-    true 
-  end
 
-# def prime?(integer)
-#     return false if integer < 2
-#     (2..integer - 1).each do |x|
-#         if (integer % x) == 0
-#             return false
-#         end
-#     end
-#     true
-# end
+def prime?(integer)
+    return false if integer < 2
+    (2..integer - 1).each do |x|
+        if (integer % x) == 0
+            return false
+        end
+    end
+    true
+end
