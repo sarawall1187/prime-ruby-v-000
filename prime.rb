75379f35763577abec3635d5cb96041293b0d06f
Sara Walker <sarawall1187@gmail.com>
2018-12-17 17:13:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,12 +3,9 @@ def prime?(max)
   while i < max
     is_divisible = ((max % i) == 0)
     if is_divisible
-      # divisor found; stop and return false!
       return false
     end
-
     i += 1
-
-end
-true 
+  end
+ true 
 end
