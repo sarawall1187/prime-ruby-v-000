d3a317961dda243e8aaf14a3f2a56392c6fa7685
Sara Walker <sarawall1187@gmail.com>
2018-12-17 17:18:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,21 +1,19 @@
-# def prime?(max)
-#   i = 2
-#   while i < max
-#     is_divisible = ((max % i) == 0)
-#     if is_divisible
-#       return false
-#     end
-#     i += 1
-#   end
-# true 
-# end
-
-def prime?(integer)
-    return false if integer < 2
-    (2..integer - 1).each do |x|
-        if (integer % x) == 0
-            return false
-        end
+def prime?(int)
+  if int < 2 
+    (2..int - 1).each do |x|
+      if (int % x) == 0 
+        return false 
+      end
     end
-    true
-end
+    true 
+  end
+
+# def prime?(integer)
+#     return false if integer < 2
+#     (2..integer - 1).each do |x|
+#         if (integer % x) == 0
+#             return false
+#         end
+#     end
+#     true
+# end
