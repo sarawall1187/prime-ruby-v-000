ad8be9b38b62eb64a10eb0de88d661f1486df6d7
Sara Walker <sarawall1187@gmail.com>
2018-12-17 17:11:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,12 @@
-def prime?(num)
+def prime?(max)
+  i = 2
+  while i < max
+    is_divisible = ((max % i) == 0)
+    if is_divisible
+      # divisor found; stop and return false!
+      return false
+    end
 
+    i += 1
 
 end
